c41f81e5932c8a92a69e2fcdef91128a55afd3bd
Michael T. Wolfinger <michael@wolfinger.eu>
2018-02-20 16:16:06 +0100

    refactored code; we can now process individual barriers/treekin landscapes AND BarMap simulations

--- coarsify_bmap.pl
+++ coarsify_bmap.pl
@@ -1,44 +1,76 @@
 #!/usr/bin/env perl
-# -*-Perl-*-
-# Last changed Time-stamp: <2018-01-04 14:39:42 mtw>
+# -*-CPerl-*-
+# Last changed Time-stamp: <2018-02-20 15:51:41 mtw>
 
-# input barriers .bar file and a treekin trajectory
+# input at least barriers .bar file and a treekin trajectory
 # output coarse grained dynamic obtained from merging lmins
+#
+# If given, regexes mark special features, only lmins with the same
+# tags can be merged.  oreach lmin list which regex in @regs matched
 
 use Getopt::Long;
 use Pod::Usage;
 use Data::Dumper;
 use File::Basename;
+use Path::Class;
+use Carp;
 use strict;
 use warnings;
 
 my $minh=3;
-my $outdir;
+my $outdir = undef;
+my $regsfile = undef;
+my $bmfile = undef;
+my $tkfile = undef;
+my @merge_lists = ();
+my @new_labels = ();
+my @regs = ();
+my @tags = ("");
 
 Getopt::Long::config("no_ignore_case");
 pod2usage(-verbose => 1)
-  unless GetOptions("minh=f"   => \$minh,
-		    "outdir=s" => \$outdir,
-		    "man"      => sub{pod2usage(-verbobose => 2)},
-		    "help"     => sub{pod2usage(-verbose => 1)});
+  unless GetOptions("minh|m=f"   => \$minh,
+		    "outdir|o=s" => \$outdir,
+		    "regs|r=s"   => \$regsfile,
+		    "barmap|b=s" => \$bmfile,
+		    "tkin|t=s"   => \$tkfile,
+		    "man"        => sub{pod2usage(-verbose => 2)},
+		    "help|h"     => sub{pod2usage(-verbose => 1)}
+		   )
+  or pod2usage( "Try '$0 --help' for more information." );
 
-my @regs = (
-	   );     # list of regexes that mark special features, only
-                  # lmins with the same tags can be merged.
-my @tags = ("");  # foreach lmin list which regex in @regs matched
+unless (defined ($tkfile)){
+  warn "Please provide an input (multi)treekin file via -t|--tkin option ...";
+  pod2usage(-verbose => 1);
+}
 
-$outdir = "coarse_$minh" unless defined($outdir);
+if(scalar(@ARGV)<1){
+  warn "Please provide least one barriers file on the command line.";
+  pod2usage(-verbose => 1 );
+}
 
+if($regsfile){
+  open my $rfh, "<", $regsfile or
+    croak "Could not open the specified regular expression file >",$regsfile,"<";
+  foreach(<$rfh>){
+    chomp;
+    push(@regs, $_)
+      if($_ !~ /^#/ and $_ !~ /^\s*$/); # ignore comment and empty lines
+  }
+  close $rfh;
+}
+
+unless (defined $outdir){
+  $outdir = "coarse_$minh";
+}
 mkdir $outdir;
-die "can't creat directory $outdir"  unless [-d $outdir];
+croak "cannot create directory $outdir" unless [-d $outdir];
 
-my @merge_lists;
-my @new_labels;
-# process the bar files
-for my $nf (0..$#ARGV-2) {
-  my ($seq, @lmin) = read_bar();
-
-  foreach (@lmin) {
+# process bar file(s)
+for my $nf (0..$#ARGV) {
+  my $curfile = $ARGV[$nf];
+  my ($seq, @lmin) = read_bar($curfile);
+  foreach (@lmin[1..$#lmin]) { # [0] is empty
     my $str = $_->[1];
     my $t = "";
     foreach my $r (@regs) {
@@ -48,7 +80,7 @@ for my $nf (0..$#ARGV-2) {
   }
 
   my @sbar = sort {$b->[4] <=> $a->[4]} @lmin[1..$#lmin];
-#  print Dumper(\@sbar);
+  # print Dumper(\@sbar);
   my @merge = ();
   $merge[$_] =$_ for (0..$#lmin);
 
@@ -61,52 +93,47 @@ for my $nf (0..$#ARGV-2) {
     $merge[$s->[0]] = $f;
   }
 
-  my $newbar = "$outdir/$ARGV";
-  open(my $fh, ">", $newbar) or die "can't open > $newbar";
+  my $bn = basename($ARGV[$nf], ".bar");
+  my $newbar = file($outdir,$bn.".bar");
+  #print "newbar ". Dumper("$newbar");
+  open(my $barfh, ">", $newbar) or carp "can't open > $newbar";
 
-  my @new = print_bar($fh, $seq, \@lmin, \@merge);  # @new contains the new lmin labels
-  close($fh);
+  my @new = print_bar($barfh, $seq, \@lmin, \@merge);  # @new contains the new lmin labels
+  close($barfh);
   push @merge_lists, \@merge;
   push @new_labels, \@new;
 }
-#print Dumper(\@merge_lists);
 
-#foreach my $m (1..14) {
-#  foreach my $l  (0..$#merge_lists) {
-#    my $n = $merge_lists[$l][$m];
-#    printf "%3d%2d ", $n, $new_labels[$l][$n] if defined($n);
-#    print "      " if !defined($n);
-#  }
-#  print "\n";
-#}
+my $bntk = basename($tkfile);
+my $newtkin = file($outdir,$bntk);
+open(my $tkfh, ">", "$newtkin") or carp "can't open > $newtkin";
+process_treekin($tkfh, $tkfile, \@merge_lists);
+close($tkfh);
 
+if($bmfile){
+  my $bnbm = basename($bmfile);
+  my $newbm = file($outdir,$bnbm);
+  open(my $bmfh, ">", "$newbm") or carp "can't open > $newbm";
+  process_barmap($bmfh, $bmfile, \@merge_lists, \@new_labels);
+  close($bmfh);
+}
 
-my $newtkin = $ARGV[-2];
-open(my $fh, ">", "$outdir/$newtkin") or die "can't open > $outdir/$newtkin";
-process_treekin($fh, \@merge_lists);
-close($fh);
-
-my $newbm = $ARGV[-1];
-open(my $fhbm, ">", "$outdir/$newbm") or die "can't open > $outdir/$newbm";
-process_barmap($fhbm, \@merge_lists, \@new_labels);
-close($fhbm);
-
-
-#---
 sub read_bar {
-  $_ = <>;
+  my ($barfile) = @_;
+  open my $bf, "<", $barfile or carp "Cannot open barriers file for reading";
+  $_ = <$bf>;
   warn "no seq in bar file $ARGV" unless /^\s+(\S+)/;
   my $seq = $1;
   my @lmin;
   my $nn = 1;
-  while (<>) {
+  while (<$bf>) {
     my @F = split;
     next if @F < 5; # shouldn't happen
-    #    splice(@F,2,1) if ($F[2] eq '('); # got 2 fields from e.g. "( -1.00)"
-    #    $F[2] =~ s/[()]//g;               # remove brackets from energy field
     $lmin[$nn++] = \@F;
     last if eof;
   }
+  close $bf;
+  #print Dumper(\@lmin);
   return $seq, @lmin;
 }
 
@@ -120,17 +147,18 @@ sub print_bar {
     #    next if $f==0; # skip unconnected
     next unless $num == $merge->[$num];
     $f = $merge->[$f] while ($merge->[$f] < $f);
-    printf $fh "%3d %s %6.2f $f%4d %6.2f\n", $n, $str, $e, $new[$f], $b;
+    printf $fh "%3d %s %6.2f %4d %6.2f\n", $n, $str, $e, $new[$f], $b;
     $new[$num] = $n++;
   }
   return @new;
 }
 
 sub process_treekin {
-  my ($fh, $merge_l) = @_;
+  my ($fh, $tkfile, $merge_l) = @_;
   my $head;
+  open my $tk, "<", $tkfile or carp "Cannot open barriers file for reading";
   foreach my $merge (@$merge_l) {
-    while (<>) {
+    while (<$tk>) {
       print($fh $_) , last if /^&/;
       next if /^@/;    # get rid of xmgrace lines 
       print($fh $_) , next if /^#/; 
@@ -139,7 +167,6 @@ sub process_treekin {
       $head=1;   # done reading headlines, a "&" will mark the end of this trajectory
       my @l = split;
       my @ll = @l;
-      
       printf $fh "%22.20e ", $l[0]; # print time
       for (my $i=$#l; $i>0; $i--) { # from right to left since we always have merge[l]<l;
 	next unless $merge->[$i];
@@ -160,17 +187,21 @@ sub process_treekin {
     undef $head;
   }
   while (!eof) { # discard rest of file
-    <>;
+    <$tk>;
   }
+  close $tk;
 }
 
 sub process_barmap {
-  my ($fh, $merge_lists, $new_labels) = @_;
-  
-  my @lines = <>;  # slurp barmap file
+  my ($fh, $bf, $merge_lists, $new_labels) = @_;
+ # print Dumper($merge_lists);
+  # print Dumper($new_labels);
+  open my $handle, "<", $bf or carp "Cannot open barmap file for reading";
+  my @lines = <$handle>;
+  close $handle;
+  #print Dumper(\@lines);
   print $fh shift(@lines);
   my @bmap;
-  
   foreach (@lines) {
     my @row = map {s/ //g; $_ eq "" ? -1 : $_ } (m/(....)(?:...)?/g);
     push @bmap, [@row];
@@ -202,21 +233,57 @@ __END__
 
 =head1 NAME
 
-coarsify_bmap.pl - coarse grain barmap trajectories for easier
-visualization and analysis
-
-=head1 SYNOPSIS
-
-coarsify_bmap.pl [--outdir out] [--minh minh] foo001.bar foo002.bar
-[foo003.bar [...]] foo.mp foo.barmap
+coarsify_bmap.pl - coarse grain barriers, treekin and barmap files, for
+better visualization and analysis
 
 =head1 DESCRIPTION
 
-coarse grain each bar file using the specified minimum barrier height
-and/or regex. Re-write the file containing the treekin trajectories
-(foo.mp) by summing up any merged states and create a new barmap file.
-The new bar files, treekin file, and barmap file are written to the
-outdir directory.
+Merge connected macro-states of a barriers files if the barrier height
+is below the selected --minh value and/or states contain similar
+structural elements, specified as regular expressions. If both
+B<--minh|-m> and B<--regs|-s> is used macro-states that contain
+different structural elements are never merged although the barrier
+height might be lower than the specified minimum. The file containing
+treekin trajectories (foo.mp) is re-written by summing up the
+population densities of any merged states. A new barmap file,
+representing the above merged macrostates is created if the
+B<--barmap|-b> option is provided . All output files are written to
+B<--outdir|-o> directory.
+
+=head1 SYNOPSIS
+
+  coarsify_bmap.pl [--minh minh] [--regs file] [--barmap foo.barmap]
+                   [--tkin foo.mp] [--outdir out]
+                   foo001.bar [foo002.bar foo003.bar [...]]
+
+=head1 OPTIONS
+
+=over 5
+
+=item B<--minh|-m>
+
+Local minima that are connected by a barrier hight lower than this
+value are merged into one minimun/basin S<(default B<3>)>.
+
+=item B<--regs|-r>
+
+File that specifies structural elements as regular expressions
+S<(default B<None>)>.
+
+=item B<--barmap|-b>
+
+Barmap output file to be processed S<(default B<None>)>.
+
+=item B<--tkin|-t>
+
+(Multi)treekin output file to be processed (B<required>). 
+
+=item B<--outdir>
+
+Specifiy the name of an output directory where all created files end
+up S<(default B<coarse_minh>)>.
+
+=back
 
 =head1 PUBLICATIONS
 
@@ -232,9 +299,15 @@ components, please cite the following publications:
  Michael T. Wolfinger, Christoph Flamm, Ivo L. Hofacker
  Methods (2018)
 
+=head1 SOURCE AVAILABITY
+
+Source code for this distribution is available from the
+L<ViennaRNA/BarMap GitHub
+repository|https://github.com/ViennaRNA/BarMap>.
+
 =head1 AUTHORS
 
-Ivo L Hofacker, Michael T Wolfinger
+Ivo L Hofacker, Michael T Wolfinger, Sven Findeiss
 
 =head1 BUGS
 
